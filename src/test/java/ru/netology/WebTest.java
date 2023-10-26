548a4c96fa6095cc6c729da752b044158aead144
Jullila <zharovayulya1997@gmail.com>
2023-10-26 21:20:23 +0500

    Test web by Selenide corrected

--- src/test/java/ru/netology/WebTest.java
+++ src/test/java/ru/netology/WebTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
-import com.codeborne.selenide.SelenideElement;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -7,31 +8,42 @@ import static com.codeborne.selenide.Condition.selected;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 public class WebTest {
+    @BeforeEach
+    public void initEach() {
+        open("http://localhost:9999");
+    }
+
     // позитивные тесты
     @Test
-    void CorrectValueTest() {
-        open("http://localhost:9999");
+    void correctValueTest() {
         $("[data-test-id=name] input").setValue("Пупкин Василий");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
     // негативные тесты
     @Test
-    void NameWithLatinTest() {
-        open("http://localhost:9999");
+    void emptyNameTest() {
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+    @Test
+    void nameWithLatinTest() {
         $("[data-test-id=name] input").setValue("sada");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
+
     @Test
-    void NameWithSymvolsTest() {
-        open("http://localhost:9999");
+    void nameWithSymvolsTest() {
         $("[data-test-id=name] input").setValue("Ма№ша");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
@@ -40,26 +52,33 @@ public class WebTest {
     }
 
     @Test
-    void PhoneLessTest() {
-        open("http://localhost:9999");
+    void emptyPhoneTest() {
+        $("[data-test-id=name] input").setValue("Маша");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+    @Test
+    void phoneLessTest() {
         $("[data-test-id=name] input").setValue("Маша");
         $("[data-test-id=phone] input").setValue("+7927000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
+
     @Test
-    void PhoneAboveTest() {
-        open("http://localhost:9999");
+    void phoneAboveTest() {
         $("[data-test-id=name] input").setValue("Маша");
         $("[data-test-id=phone] input").setValue("+792700000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
+
     @Test
-    void PhoneWitoutPlusTest() {
-        open("http://localhost:9999");
+    void phoneWitoutPlusTest() {
         $("[data-test-id=name] input").setValue("Маша");
         $("[data-test-id=phone] input").setValue("792700000000");
         $("[data-test-id=agreement]").click();
@@ -68,8 +87,7 @@ public class WebTest {
     }
 
     @Test
-    void WithoutAcceptTest() {
-        open("http://localhost:9999");
+    void withoutAcceptTest() {
         $("[data-test-id=name] input").setValue("Маша");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("button.button").click();
